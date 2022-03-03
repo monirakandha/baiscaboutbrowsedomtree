adfe83d3bc6b6adf4a94e11135ea14cfbf53d4c7
monirhosenakanda <monirhosenakanda@gmail.com>
2022-03-03 13:35:59 +0600

    first commit

---
+++ timeout.js
@@ -0,0 +1,17 @@
+function doSomething() {
+    console.log('I am coding');
+}
+
+console.log('AMi sobar age');
+console.log('I am the second Person');
+setTimeout(doSomething, 5000)
+console.log('Ami 3 number baccha');
+setTimeout(function(){
+    console.log('I am using VS code');
+} ,7000)
+setTimeout(() => {
+    console.log("exploring mdn articale")
+} , 8000)
+console.log('ami 3k hali purno korchi');
+
+
